da2ca92af4cc34fb70e030475257bf8398d53e4c
Jacob Chatfield <jacob@hoverbear.org>
2020-07-27 08:45:53 -0700

    Update LICENSE file

--- rolly.py
+++ rolly.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     parser.add_argument('die', type=str, nargs='+', help='a die to roll')
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 
-    rolls = parser.parse_args()
+    rolls = parser.parse_args()  # Parses command line input
 
-    # Isolates individual rolls
+
     for key in vars(rolls):
-        for roll in getattr(rolls, key):
+        for roll in getattr(rolls, key):  # Isolates individual rolls
             result = dice.Roll(roll)
             print("Your {} roll resulted in: {}\nThe individual rolls and bonus modifier were: ({}) + {}".format(roll, sum(result.roll_results) + result.roll_bonus, result.pretty_print(), result.roll_bonus))
