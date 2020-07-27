008fef8a9c7926a76d3575f9f9d89023da5f4062
Jacob Chatfield <jacob@hoverbear.org>
2020-07-27 07:27:09 -0700

    Initialize

---
+++ dice.py
@@ -0,0 +1,61 @@
+import random
+
+class Roll:
+    num_die = 0
+    die_sides = 0
+    roll_bonus = 0
+    roll_results = []
+    def __init__(self, roll_req):
+        '''
+
+        :param roll_req: The input string formatted, for example, 2d2+1
+
+        >>> random.seed(1)
+        >>> roll_req = Roll("4d10-2")
+        >>> roll_req.num_die
+        4
+        >>> roll_req.die_sides
+        10
+        >>> roll_req.roll_bonus
+        -2
+        >>> roll_req.die()
+        8
+        >>> roll_req.roll_results
+        [3, 2, 5, 2]
+        >>> roll_req.pretty_print()
+        '3 + 2 + 5 + 2'
+        >>> Roll("2e20").num_die
+        Traceback (most recent call last):
+          ...
+        IndexError: list index out of range
+
+        '''
+        # Reads and separates the incoming roll request
+        str_reader = roll_req.split("d")
+        if str_reader[0].isdigit():  # Number of dice to roll
+            self.num_die = int(str_reader[0])
+        else:
+            self.num_die = 1
+
+        if "+" in str_reader[1]:  # Determines presence of a positive modifier
+            str_reader.append(str_reader[1].split("+"))
+            self.roll_bonus = int(str_reader[2][1])  # Roll with a positive modifier
+            self.die_sides = int(str_reader[2][0])  # Number of sides on die
+
+        elif "-" in str_reader[1]:  # Determines presence of a negative modifier
+            str_reader.append(str_reader[1].split("-"))
+            self.roll_bonus = 0 - int(str_reader[2][1])  # Roll with a negative modifier
+            self.die_sides = int(str_reader[2][0])  # Number of sides on die
+
+        else:  # No roll modifier present
+            self.die_sides = int(str_reader[1])  # Number of sides on die
+
+        for self.i in range(self.num_die):  # Dice roll
+            self.roll_results.append(self.die())
+
+    def die(self):
+        return random.randrange(1, self.die_sides)  # Die roll
+
+    def pretty_print(self):  # Makes a math formula
+        self.pretty = " + ".join(map(str, self.roll_results))
+        return self.pretty
---
+++ rolly.py
@@ -0,0 +1,16 @@
+import argparse
+import dice
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='Rolly', description='Roll some dice!')
+
+    parser.add_argument('die', type=str, nargs='+', help='a die to roll')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+    rolls = parser.parse_args()
+
+    # Isolates individual rolls
+    for key in vars(rolls):
+        for roll in getattr(rolls, key):
+            result = dice.Roll(roll)
+            print("Your {} roll resulted in: {}\nThe individual rolls and bonus modifier were: ({}) + {}".format(roll, sum(result.roll_results) + result.roll_bonus, result.pretty_print(), result.roll_bonus))
